e7bf550fa3ddec79233e3154863f7faaaac2ab33
Pingu <jesper@acorneroftheweb.com>
2022-06-04 23:15:30 +0200

    Formatted somewhat

--- main.lisp
+++ main.lisp
@@ -5,24 +5,27 @@
             :initarg :path
             :type string
             :documentation "The files path")
-  (modified :accessor file-modified
-            :initarg :modified
-            :initform 0
-            :type (integer 0)
-            :documentation "The last time the file was modified"))
+   (modified :accessor file-modified
+             :initarg :modified
+             :initform 0
+             :type (integer 0)
+             :documentation "The last time the file was modified"))
   (:documentation "The base class of files."))
 
 (defun list-all-files (root-path)
-  (apply #'concatenate 'list (map 'list #'(lambda (file-path)
-        (make-instance  'file
-                        :path file-path
-                        :modified
-                          (osicat-posix:stat-mtime (osicat-posix:stat file-path))))
-        (uiop:directory-files root-path))
-      (remove-if #'(lambda (x)
-                    (equal x NIL))
-                 (concatenate 'list (map 'list #'list-all-files
-                    (uiop:subdirectories root-path))))))
+  (concatenate 'list
+               (map 'list #'(lambda (file-path)
+                              (make-instance  'file
+                                              :path file-path
+                                              :modified
+                                              (osicat-posix:stat-mtime
+                                               (osicat-posix:stat file-path))))
+                    (uiop:directory-files root-path))
+               (remove-if
+                #'(lambda (x) (equal x NIL))
+                (concatenate 'list
+                             (map 'list #'list-all-files
+                                  (uiop:subdirectories root-path))))))
 
 (defun main ()
-    (format t "Hello there"))
+  (format t "Hello there"))
